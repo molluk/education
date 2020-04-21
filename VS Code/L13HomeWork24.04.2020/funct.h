8a31c3626f1270918351ad556f5c8d89bed49b16
Arthur <molluk@mail.ru>
2020-04-21 19:26:59 +0300

    21.04.2020 main.cpp kak perekinyt string iz main.cpp v drygoi file?

--- VS Code/L13HomeWork24.04.2020/funct.h
+++ VS Code/L13HomeWork24.04.2020/funct.h
@@ -4,11 +4,7 @@
 /* ###|   Created: 2020-04-18 16:25:52   |### */
 /* ########################################## */
 
+#pragma once
 #include <iostream>
 
 using namespace std;
-
-char     random_book_name(int random);
-char     random_author(int random, int random_gender);
-char     random_publishing_house(int random);
-char     random_genre(int random);
